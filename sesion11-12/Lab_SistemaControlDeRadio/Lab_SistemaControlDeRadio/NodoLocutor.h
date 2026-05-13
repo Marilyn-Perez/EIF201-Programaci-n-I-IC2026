5f372ff70a99242411135dc4c99af4ef5c3ccfc4
Marilyn Perez Sequeira <Marilyn.perez.sequeira@est.una.ac.cr>
2026-05-13 16:55:25 -0600

    Implementacion de lista circular: rotacion de locutores

--- sesion11-12/Lab_SistemaControlDeRadio/Lab_SistemaControlDeRadio/NodoLocutor.h
+++ sesion11-12/Lab_SistemaControlDeRadio/Lab_SistemaControlDeRadio/NodoLocutor.h
@@ -1,5 +1,26 @@
-#pragma once
-class NodoLocutor
-{
-};
+#ifndef NODOLOCUTOR_H
+#define NODOLOCUTOR_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+namespace EIF201 {
+
+	struct NodoLocutor {
+		std::string nombre;
+		NodoLocutor* siguiente;
+
+		NodoLocutor(const std::string& n)
+			: nombre(n), siguiente(nullptr) {
+			cout << "[NodoLocutor creado: " << n << "]" << endl;
+		}
+		~NodoLocutor() {
+			cout << "[NodoLocutor destruido: " << nombre << "]" << endl;
+		}
+	};
+
+}
+
+#endif
--- sesion11-12/Lab_SistemaControlDeRadio/Lab_SistemaControlDeRadio/RotacionCircular.h
+++ sesion11-12/Lab_SistemaControlDeRadio/Lab_SistemaControlDeRadio/RotacionCircular.h
@@ -1,5 +1,42 @@
-#pragma once
-class RotacionCircular
-{
-};
+#ifndef ROTACIONCIRCULAR_H
+#define ROTACIONCIRCULAR_H
 
+#include "NodoLocutor.h"
+
+namespace EIF201 {
+
+	class RotacionCircular {
+	private:
+		NodoLocutor* cola;
+		int cantidad;
+
+		NodoLocutor* getCabeza() const;
+
+	public:
+		RotacionCircular();
+		~RotacionCircular();
+
+		void insertarAlFinal(const std::string& nombre);
+		void insertarAlInicio(const std::string& nombre);
+		bool insertarDespuesDe(const std::string& referencia, const std::string& nuevo_nombre);
+
+		bool existeLocutor(const std::string& nombre) const;
+		int obtenerPosicion(const std::string& nombre) const;
+
+		std::string turnoActual() const;
+
+		bool eliminarLocutor(const std::string& nombre);
+		bool eliminarTurnoActual();
+
+		std::string siguiente();
+
+		void simularTurnos(int cantidadTurnos);
+		void imprimirRotacion() const;
+
+		bool estaVacia() const;
+		int getCantidad() const;
+	};
+
+}
+
+#endif
